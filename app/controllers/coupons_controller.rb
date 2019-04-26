5585a7d9acc70e4b84daa52875cc9ff6409c2c1a
Tyler Hunt <tyhunt04@gmail.com>
2019-04-26 03:06:59 +0000

    Automatically backed up by Learn

---
+++ app/controllers/coupons_controller.rb
@@ -0,0 +1,23 @@
+class CouponsController < ApplicationController
+
+  def index
+    @coupons = Coupon.all
+  end
+
+  def new
+    @coupon = Coupons.new
+  end
+
+  def create
+    @coupon = Coupon.new
+    @coupon.coupon_code = params[:coupon][:coupon_code]
+    @coupon.store = params[:coupon][:store]
+    @coupon.save
+    redirect_to coupons_path(@coupon)
+  end
+
+  def show
+    @coupon = Coupon.find(params[:id])
+  end
+
+end
